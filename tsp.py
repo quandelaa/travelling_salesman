dc829930553d71b5752ca4f22fa29f8dd83e1d15
quandelaa <sergiotobamasilalahi@gmail.com>
2026-04-12 16:35:56 +0700

    refactor: functions stored in class Algos and make the path choose its directory randomly (between row, col and col, row)

--- tsp.py
+++ tsp.py
@@ -1,28 +1,54 @@
-def mha_distance(point_a, point_b):
-    return abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1])
+from random import randint
 
-def walk(start_pos, end_pos):
-    path = []
+class Algos:
+    def __init__(self):
+        self.path = []
+        self.cur_c, self.cur_r = None, None
+        
+    def mha_distance(self, point_a, point_b):
+        return abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1])
 
-    c1, r1 = start_pos
-    c2, r2 = end_pos
+    def mha_distance_for_overall_path(self, overall_path):
+        overall_distance = 0
+            
+        for i in range(len(overall_path)-1):
+            overall_distance += self.mha_distance(overall_path[i], overall_path[i+1])
 
-    cur_r, cur_c = r1, c1
+        return overall_distance
 
-    while cur_r != r2:
-        if r1 > r2:
-            cur_r -= 1
+    def walk(self, start_pos, end_pos):
+        self.path.clear()
+
+        self.c1, self.r1 = start_pos
+        self.c2, self.r2 = end_pos
+
+        chooser = randint(0,1)
+
+        self.cur_r, self.cur_c = self.r1, self.c1
+        
+        if chooser:
+            self.row()
+            self.col()
         else:
-            cur_r += 1
+            self.col()
+            self.row()    
 
-        path.append((cur_c, cur_r))
+        return self.path
 
-    while cur_c != c2:
-        if c1 > c2:
-            cur_c -= 1
-        else:
-            cur_c += 1
+    def row(self):
+        while self.cur_r != self.r2:
+            if self.r1 > self.r2:
+                self.cur_r -= 1
+            else:
+                self.cur_r += 1
 
-        path.append((cur_c, cur_r))
+            self.path.append((self.cur_c, self.cur_r))
 
-    return path
+    def col(self):
+        while self.cur_c != self.c2:
+            if self.c1 > self.c2:
+                self.cur_c -= 1
+            else:
+                self.cur_c += 1
+
+            self.path.append((self.cur_c, self.cur_r))
