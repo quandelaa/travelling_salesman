3ebcf83294f3ddb0990bd268a1d72a33047f64e1
quandelaa <sergiotobamasilalahi@gmail.com>
2026-04-13 20:07:04 +0700

    refactor: saved hardcoded values to class instance variables and made design improvements

--- runner.py
+++ runner.py
@@ -18,6 +18,18 @@ class TSP:
 
         self.tsp = Algos()
 
+        self.BLACK = "black"
+        self.WHITE = "white"
+        self.GREEN = "green"
+        self.RED = "red"
+        self.BLUE = "blue"
+        self.ORANGEISH = "#EFFF84"
+
+        self.SIZE_XY = 40
+        self.ROW_SIZE = 20
+
+        self.TILE_ANIM_DELAY = 0.05
+        
         self.font = ("Verdana", 18)
         self.toggled = "city"
 
@@ -31,17 +43,17 @@ class TSP:
         self.rectangles_size = {}
 
         for i in range(300):
-            col = i % 20
-            row = i // 20
+            col = i % self.ROW_SIZE
+            row = i // self.ROW_SIZE
 
-            x_pos = col * 40
-            y_pos = row * 40
+            x_pos = col * self.SIZE_XY
+            y_pos = row * self.SIZE_XY
             
-            x_size = x_pos+40
-            y_size = y_pos+40
+            x_size = x_pos+self.SIZE_XY
+            y_size = y_pos+self.SIZE_XY
 
-            self.rectangles.create_rectangle(x_pos, y_pos, x_size, y_size, fill="black", outline="white", tags=("clickable", str(i)))
-            self.rectangles_size[i] = (x_pos, y_pos, x_size, y_size)
+            self.rectangles.create_rectangle(x_pos, y_pos, x_size, y_size, fill=self.BLACK, outline=self.WHITE, tags=("clickable", str(i)))
+            self.rectangles_size[i+1] = (x_pos, y_pos, x_size, y_size)
 
         self.rectangles.bind("<Button-1>", self.on_click)
         self.rectangles.bind("<Button-3>", self.on_declick)
@@ -49,20 +61,20 @@ class TSP:
         self.rectangles.pack()
     
     def setup_manager(self):
-        manager_window = tk.Toplevel(bg="black")
+        manager_window = tk.Toplevel(bg=self.BLACK)
     
         manager_window.resizable(width=False, height=False) 
         manager_window.title("Grid Manager")
         
-        city_toggle = tk.Button(manager_window, text="CITY", font=self.font, fg="black", bg="white", command=lambda: self.toggle("city"))
-        start_toggle = tk.Button(manager_window, text="START", font=self.font, fg="black", bg="white", command=lambda: self.toggle("start"))
-        end_toggle = tk.Button(manager_window, text="END", font=self.font, fg="black", bg="white", command=lambda: self.toggle("end"))
+        city_toggle = tk.Button(manager_window, text="CITY", font=self.font, fg=self.BLACK, bg=self.WHITE, command=lambda: self.toggle("city"))
+        start_toggle = tk.Button(manager_window, text="START", font=self.font, fg=self.BLACK, bg=self.WHITE, command=lambda: self.toggle("start"))
+        end_toggle = tk.Button(manager_window, text="END", font=self.font, fg=self.BLACK, bg=self.WHITE, command=lambda: self.toggle("end"))
 
-        self.mode_text = tk.Label(manager_window, text="MODE: CITY", font=self.font, bg="black", fg="white")
-        solve_button = tk.Button(manager_window, text="SOLVE", font=self.font, fg="black", bg="white", command=self.solve)
-        clear_button = tk.Button(manager_window, text="CLEAR", font=self.font, fg="black", bg="white", command=self.clear)
+        self.mode_text = tk.Label(manager_window, text="MODE: CITY", font=self.font, bg=self.BLACK, fg=self.WHITE)
+        solve_button = tk.Button(manager_window, text="SOLVE", font=self.font, fg=self.BLACK, bg=self.WHITE, command=self.solve)
+        clear_button = tk.Button(manager_window, text="CLEAR", font=self.font, fg=self.BLACK, bg=self.WHITE, command=self.clear)
 
-        self.cur_distance_text = tk.Label(manager_window, text="DISTANCE: 0 TILES", font=self.font, bg="black", fg="white")
+        self.cur_distance_text = tk.Label(manager_window, text="DISTANCE: 0 TILES", font=self.font, bg=self.BLACK, fg=self.WHITE)
 
         start_toggle.pack(fill="x")
         end_toggle.pack(fill="x")
@@ -103,17 +115,31 @@ class TSP:
         with self.solving_lock:
             for i in range(len(overall_path)-1):    
                 path = self.tsp.walk(overall_path[i], overall_path[i+1])
-                self.path_colorize(path)
+                self.path_colorize(path, overall_path)
     
-    def path_colorize(self, path):
+    def path_colorize(self, path, overall_path):
         for point in path:
             col, row = point
-            id = ((row)*20)+col+1
+            id = ((row)*self.ROW_SIZE)+col+1
 
-            if self.rectangles.itemcget(id, "fill") == "black":
-                self.rectangles.itemconfig(id, fill="blue")
+            if self.rectangles.itemcget(id, "fill") == self.BLACK:
+                self.rectangles.itemconfig(id, fill=self.BLUE)
+            elif self.rectangles.itemcget(id, "fill") == self.WHITE:
+                if point in overall_path:
+                    i = overall_path.index(point)
+                    
+                    if i > 1:
+                        col, row = overall_path[i-1]
+                        cur_col, cur_row = overall_path[i]
+                        id = ((row)*self.ROW_SIZE)+col+1
+                        cur_id = ((cur_row)*self.ROW_SIZE)+cur_col+1
 
-            sleep(0.1)
+                        if self.rectangles.itemcget(id, "fill") == self.ORANGEISH:
+                            self.rectangles.itemconfig(cur_id, fill=self.ORANGEISH)
+                    else:
+                        self.rectangles.itemconfig(id, fill=self.ORANGEISH)
+
+            sleep(self.TILE_ANIM_DELAY)
             
     def on_declick(self, event):        
         if self.solving_lock.locked():
@@ -121,34 +147,35 @@ class TSP:
             return
 
         with self.solving_lock:
-            col = event.x // 40
+            col = event.x // self.SIZE_XY
             row = event.y // 40
             
-            id = ((row)*20)+col+1
+            id = ((row)*self.ROW_SIZE)+col+1
 
             color = self.rectangles.itemcget(id, "fill")
 
-            if color == "black":
-                print("not clicked yet", id)
+            if color == self.BLACK:
+                print(f"tile {id} not clicked yet")
                 return
-            elif color == "blue":
-                print("path can not be deleted", id)
+            elif color == self.BLUE:
+                print("path can not be deleted")
                 return
             
-            if color == "green":
+            if color == self.GREEN:
                 self.start = None
                 print("start:",self.start)
-            elif color == "red":
+            elif color == self.RED:
                 self.end = None
                 print("end:",self.end)
-            elif color == "white":
+            elif color == self.WHITE or color == self.ORANGEISH:
                 self.cities.remove((col, row))
-                self.change_city_text()
 
                 print("cities:",self.cities)
 
-            self.rectangles.itemconfig(id, fill="black")
-            self.rectangles.itemconfig(id, outline="white")
+            self.change_city_text()
+
+            self.rectangles.itemconfig(id, fill=self.BLACK)
+            self.rectangles.itemconfig(id, outline=self.WHITE)
 
             print("declicked", id)
 
@@ -158,28 +185,28 @@ class TSP:
             return
 
         with self.solving_lock:
-            col = event.x // 40
-            row = event.y // 40
+            col = event.x // self.SIZE_XY
+            row = event.y // self.SIZE_XY
             
-            id = ((row)*20)+col+1
+            id = ((row)*self.ROW_SIZE)+col+1
 
             color = self.rectangles.itemcget(id, "fill")
             color_config = None
 
-            if color != "black":
+            if color != self.BLACK:
                 print("already clicked", id)
                 return
 
             if self.toggled == "city":
-                color_config = "white"
+                color_config = self.WHITE
                 self.cities.append((col, row))
 
                 print("cities:", self.cities)
 
-                self.create_city_text(col, row, id-1)
+                self.create_city_text(col, row, id)
             
             elif self.toggled == "start":
-                color_config = "green"
+                color_config = self.GREEN
                 if self.start is None:
                     self.start = (col, row)
                     print("start:", self.start)
@@ -187,7 +214,7 @@ class TSP:
                     self.warning("Start point can only be one")
                     return
             elif self.toggled == "end":
-                color_config = "red"
+                color_config = self.RED
                 if self.end is None:
                     self.end = (col, row)
                     print("end:", self.end)
@@ -214,7 +241,7 @@ class TSP:
         for point in self.cities:
             col, row = point
             
-            id = (row*20)+col
+            id = (row*self.ROW_SIZE)+col+1
 
             self.create_city_text(col, row, id)
 
@@ -232,9 +259,12 @@ class TSP:
 
         with self.solving_lock:
             for id in range(1,301):
-                if self.rectangles.itemcget(id, "fill") == "blue":
-                    self.rectangles.itemconfig(id, fill="black")
-                    self.rectangles.itemconfig(id, outline="white")
+                if self.rectangles.itemcget(id, "fill") == self.BLUE:
+                    self.rectangles.itemconfig(id, fill=self.BLACK)
+                    self.rectangles.itemconfig(id, outline=self.WHITE)
+                elif self.rectangles.itemcget(id, "fill") == self.ORANGEISH:
+                    self.rectangles.itemconfig(id, fill=self.WHITE)
+                    self.rectangles.itemconfig(id, outline=self.BLACK)    
 
 if __name__ == "__main__":
     window = tk.Tk()
